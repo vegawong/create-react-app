e48bcfe3f6e3486262221da68145e32fdf73ebdd
vega <vegawong1024@gmail.com>
2019-03-22 16:22:50 +0800

    fix: stylelint配置顺序问题

--- packages/react-scripts/template-typescript/.stylelintrc.js
+++ packages/react-scripts/template-typescript/.stylelintrc.js
@@ -1,10 +1,6 @@
 // @ts-nocheck
 module.exports = {
     ignoreFiles: ['**/*.md', '**/*.ts', '**/*.tsx', '**/*.js'],
-    extends: ['stylelint-config-css-modules', 'stylelint-config-standard', 'stylelint-config-prettier'],
-    rules: {
-        'indentation': [4],
-        'property-no-unknown': [true, { ignoreProperties: ['composes'] }]
-    }
+    extends: ['stylelint-config-standard', 'stylelint-config-css-modules', 'stylelint-config-prettier']
 }
 
